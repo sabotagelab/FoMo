77383d3f071083407d9bb6a6377865d639a23bbc
Colin Shea-Blymyer <c0lin@vt.edu>
2022-09-21 16:01:29 -0700

    Filled out data analysis, updated gitignore to include an ad hoc stats summary.

--- data_analysis.py
+++ data_analysis.py
@@ -17,7 +17,10 @@ import multiprocessing
 import csv
 import re
 
+from collections import defaultdict, Counter
+
 import matplotlib.pyplot as plt
+import numpy as np
 from tqdm import trange, tqdm
 from joblib import Parallel, delayed
 from nltk import ChartParser
@@ -27,15 +30,45 @@ from boss.code.CFG.CFG import Grammar
 from bayes_opt import deformat
 
 
-def num_prod_rules(grammar, csvfile):
-    datareader = csv.reader(csvfile)
-    parser = ChartParser(grammar)
-    for row in datareader:
-        phi = row[1]
-        phi_tokens = re.split(r"\s|([()!])", phi)
-        phi_tokens = [deformat(c) for c in phi_tokens if c]
-        phi_tree = parser.parse(phi_tokens)
-        # TODO: work with that tree
+def num_prod_rules(formula_trees):
+    prods_parse = []
+    for parse_tree in formula_trees:
+        prods_parse.append(len(parse_tree.productions()))
+    return min(prods_parse)
+
+
+def get_operators(formula_tokens, non_ops):
+    op_tokens = [token for token in formula_tokens if token not in non_ops]
+    return op_tokens
+
+
+def get_props(formula_tokens, props):
+    prop_tokens = [token for token in formula_tokens if token in props]
+    return prop_tokens
+
+
+def plot_count(count_dict, ylabel, xlabel, save_path):
+    plt.figure()
+    plt.bar(count_dict.keys(), count_dict.values())
+    plt.ylabel(ylabel)
+    plt.xlabel(xlabel)
+    plt.savefig(save_path)
+
+
+def plot_counts(count_dicts, ylabel, xlabel, save_path):
+    plt.figure()
+    for count_dict in count_dicts:
+        plt.bar(count_dict.keys(), count_dict.values())
+    plt.ylabel(ylabel)
+    plt.xlabel(xlabel)
+    plt.savefig(save_path)
+
+
+def count_to_list(count_dict):
+    count_list = []
+    for key in count_dict.keys():
+        count_list.extend([key]*count_dict[key])
+    return count_list
 
 
 if __name__ == "__main__":
@@ -54,7 +87,129 @@ if __name__ == "__main__":
         UNTIL -> "U"
         T -> """
     propositions = ['p', 'q', 'r', 's', 'a', 'b', 'c', 'd', 'e', 'f', 'g']
+    non_operators = propositions + ['lb', 'rb']
     prop_str = " | ".join(['"' + proposition + '"' for proposition in propositions])
     grammar_str = grammar_str + prop_str
     ltl_grammar = Grammar.fromstring(grammar_str)
+    ltl_parser = ChartParser(ltl_grammar)
+    csvpath = f"/home/colin/Documents/GitHub/deep_verify/data/deep_verify_eval_data.csv"
 
+    terminals_count = defaultdict(lambda: 0, {})
+    prod_rules_count = defaultdict(lambda: 0, {})
+    operators_count = defaultdict(lambda: 0, {})
+    props_count = defaultdict(lambda: 0, {})
+    each_terms_count = Counter()
+
+    sat_terminals_count = defaultdict(lambda: 0, {})
+    sat_prod_rules_count = defaultdict(lambda: 0, {})
+    sat_operators_count = defaultdict(lambda: 0, {})
+    sat_props_count = defaultdict(lambda: 0, {})
+    sat_each_terms_count = Counter()
+
+    unsat_terminals_count = defaultdict(lambda: 0, {})
+    unsat_prod_rules_count = defaultdict(lambda: 0, {})
+    unsat_operators_count = defaultdict(lambda: 0, {})
+    unsat_props_count = defaultdict(lambda: 0, {})
+    unsat_each_terms_count = Counter()
+
+    with open(csvpath, 'r', newline='') as csvfile:
+        datareader = csv.DictReader(csvfile)
+        for row in tqdm(datareader):
+            phi = row["phi"]
+            label = row["label"]
+            phi_tokens = re.split(r"\s|([()!])", phi)
+            phi_tokens = [deformat(c) for c in phi_tokens if c]
+            phi_trees = [tree for tree in ltl_parser.parse(phi_tokens)]
+            # count the number of terminals in this formula
+            phi_term_count = len(phi_tokens)
+            terminals_count[phi_term_count] += 1
+            # get the number of production rules for this formula
+            if phi_trees:
+                phi_prod_rules_count = num_prod_rules(phi_trees)
+                prod_rules_count[phi_prod_rules_count] += 1
+            # get the number of operators
+            phi_operators = get_operators(phi_tokens, non_operators)
+            phi_operators_count = len(phi_operators)
+            operators_count[phi_operators_count] += 1
+            # get the number of atomic propositions
+            phi_props = get_props(phi_tokens, propositions)
+            phi_props_count = len(phi_props)
+            props_count[phi_props_count] += 1
+            # count the number of each terminal
+            phi_terms_count = Counter(phi_tokens)
+            each_terms_count.update(phi_terms_count)
+            if label == "1":
+                sat_terminals_count[phi_term_count] += 1
+                if phi_trees:
+                    sat_prod_rules_count[phi_prod_rules_count] += 1
+                sat_operators_count[phi_operators_count] += 1
+                sat_props_count[phi_props_count] += 1
+                sat_each_terms_count.update(phi_terms_count)
+            else:
+                unsat_terminals_count[phi_term_count] += 1
+                if phi_trees:
+                    unsat_prod_rules_count[phi_prod_rules_count] += 1
+                unsat_operators_count[phi_operators_count] += 1
+                unsat_props_count[phi_props_count] += 1
+                unsat_each_terms_count.update(phi_terms_count)
+
+    # plot data
+    fig_dir = f"data/"
+    plot_count(prod_rules_count, "Number of formulas", "Number of production rules", fig_dir+"prod_rules_count")
+    plot_counts([sat_prod_rules_count, unsat_prod_rules_count], "Number of formulas", "Number of production rules",
+                fig_dir+"compare_prod_rules_count")
+    plot_count(terminals_count, "Number of formulas", "Formula length", fig_dir+"terminals_count")
+    plot_counts([sat_terminals_count, unsat_terminals_count], "Number of formulas", "Formula length",
+                fig_dir+"compare_terminals_count")
+    plot_count(operators_count, "Number of formulas", "Number of operators", fig_dir+"operators_count")
+    plot_counts([sat_operators_count, unsat_operators_count], "Number of formulas", "Number of operators",
+                fig_dir + "compare_operators_count")
+    plot_count(props_count, "Number of formulas", "Number of propositions", fig_dir+"propositions_count")
+    plot_counts([sat_props_count, unsat_props_count], "Number of formulas", "Number of propositions",
+                fig_dir+"compare_propositions_count")
+    plot_count(each_terms_count, "Number of each terminal", "Terminal symbol", fig_dir+"each_terms_count")
+    plot_counts([sat_each_terms_count, unsat_each_terms_count], "Number of each terminal", "Terminal symbol",
+                fig_dir + "compare_each_terms_count")
+
+    # calculate summary statistics
+    prod_rules_count_list = count_to_list(prod_rules_count)
+    sat_prod_rules_count_list = count_to_list(sat_prod_rules_count)
+    unsat_prod_rules_count_list = count_to_list(unsat_prod_rules_count)
+    print("Mean number of production rules per formula: " + str(np.mean(prod_rules_count_list)) +
+          "; STD:" + str(np.std(prod_rules_count_list)))
+    print("Mean number of production rules per satisfied formula: " + str(np.mean(sat_prod_rules_count_list)) +
+          "; STD:" + str(np.std(sat_prod_rules_count_list)))
+    print("Mean number of production rules per unsatisfied formula: " + str(np.mean(unsat_prod_rules_count_list)) +
+          "; STD:" + str(np.std(unsat_prod_rules_count_list)))
+
+    terminals_count_list = count_to_list(terminals_count)
+    sat_terminals_count_list = count_to_list(sat_terminals_count)
+    unsat_terminals_count_list = count_to_list(unsat_terminals_count)
+    print("Mean number of terminals per formula: " + str(np.mean(terminals_count_list)) +
+          "; STD:" + str(np.std(terminals_count_list)))
+    print("Mean number of terminals per satisfied formula: " + str(np.mean(sat_terminals_count_list)) +
+          "; STD:" + str(np.std(sat_terminals_count_list)))
+    print("Mean number of terminals per unsatisfied formula: " + str(np.mean(unsat_terminals_count_list)) +
+          "; STD:" + str(np.std(unsat_terminals_count_list)))
+
+    operators_count_list = count_to_list(operators_count)
+    sat_operators_count_list = count_to_list(sat_operators_count)
+    unsat_operators_count_list = count_to_list(unsat_operators_count)
+    print("Mean number of operators per formula: " + str(np.mean(operators_count_list)) +
+          "; STD:" + str(np.std(operators_count_list)))
+    print("Mean number of operators per satisfied formula: " + str(np.mean(sat_operators_count_list)) +
+          "; STD:" + str(np.std(sat_operators_count_list)))
+    print("Mean number of operators per unsatisfied formula: " + str(np.mean(unsat_operators_count_list)) +
+          "; STD:" + str(np.std(unsat_operators_count_list)))
+
+    props_count_list = count_to_list(props_count)
+    sat_props_count_list = count_to_list(sat_props_count)
+    unsat_props_count_list = count_to_list(unsat_props_count)
+    print("Mean number of atomic propositions per formula: " + str(np.mean(props_count_list)) +
+          "; STD:" + str(np.std(props_count_list)))
+    print("Mean number of atomic propositions per satisfied formula: " + str(np.mean(sat_props_count_list)) +
+          "; STD:" + str(np.std(sat_props_count_list)))
+    print("Mean number of atomic propositions per unsatisfied formula: " + str(np.mean(unsat_props_count_list)) +
+          "; STD:" + str(np.std(unsat_props_count_list)))
+
+    print("done")
